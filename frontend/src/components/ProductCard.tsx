830ad367c479f769f3c23ea9543a07b14b9135dd
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-10-18 16:37:34 +0800

    style: change appearance of advertisement card

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,48 +1,86 @@
-import {
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
-  Box,
-} from '@mui/material';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Card, CardMedia, CardContent, Typography, Box } from '@mui/material';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
-type ProductCardProps = {
-  image: string;
+interface ProductCardProps {
+  imageUrl: string;
   title: string;
-  price: number;
-  views: number;
-  likes: number;
-};
+  originalPrice: number;
+  viewCount: number;
+  likeCount: number;
+}
 
-function ProductCard({ image, title, price, views, likes }: ProductCardProps) {
+function ProductCard({
+  imageUrl,
+  title,
+  originalPrice,
+  viewCount,
+  likeCount,
+}: ProductCardProps) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" height="140" image={image} alt={title} />
-      <CardContent>
-        <Typography gutterBottom variant="h6" component="div">
+    <Card
+      sx={{
+        maxWidth: 345,
+        backgroundColor: 'white',
+        borderRadius: 2,
+        boxShadow: 0,
+        transition: 'box-shadow 0.3s ease-in-out',
+        '&:hover': {
+          boxShadow: 3,
+        },
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={imageUrl}
+        alt={title}
+        sx={{
+          width: '100%',
+          aspectRatio: '1 / 1',
+          objectFit: 'cover',
+        }}
+      />
+      <CardContent
+        sx={{
+          padding: 0,
+        }}
+      >
+        <Typography gutterBottom variant="subtitle1" component="div" noWrap>
           {title}
         </Typography>
-        <Typography variant="h5" color="text.primary">
-          ${price}
-        </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <Box display="flex" alignItems="center" mr={2}>
-          <VisibilityIcon fontSize="small" />
-          <Typography variant="body2" color="text.secondary" ml={0.5}>
-            {views}
-          </Typography>
-        </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mx: 2,
+          mb: 1,
+        }}
+      >
+        <Typography variant="h5" color="text.primary">
+          ${originalPrice}
+        </Typography>
+
         <Box display="flex" alignItems="center">
-          <FavoriteIcon fontSize="small" color="error" />
-          <Typography variant="body2" color="text.secondary" ml={0.5}>
-            {likes}
-          </Typography>
+          <Box display="flex" alignItems="center" mr={2}>
+            <VisibilityOutlinedIcon fontSize="small" />
+            <Typography variant="body2" color="text.secondary" ml={0.5}>
+              {viewCount}
+            </Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <FavoriteIcon sx={{ color: 'red' }} fontSize="small" />
+            <Typography variant="body2" color="text.secondary" ml={0.5}>
+              {likeCount}
+            </Typography>
+          </Box>
         </Box>
-      </CardActions>
+      </Box>
     </Card>
   );
 }
