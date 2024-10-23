f41d9a18676ee9836d80b876f7e68f0b81cd8e6d
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-10-23 15:02:24 +0800

    feat: add pages templates

---
+++ frontend/src/pages/AdvertisementDetailsPage.tsx
@@ -0,0 +1,7 @@
+import Layout from '../layouts/layout';
+
+function AdvertisementDetailsPage() {
+  return <Layout>Advertisement Details Page</Layout>;
+}
+
+export default AdvertisementDetailsPage;
---
+++ frontend/src/pages/OrdersPage.tsx
@@ -0,0 +1,7 @@
+import Layout from '../layouts/layout';
+
+function OrdersPage() {
+  return <Layout>Orders Page</Layout>;
+}
+
+export default OrdersPage;
